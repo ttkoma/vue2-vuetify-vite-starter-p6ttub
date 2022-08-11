a8a2de914c43b3c1800cfd795c91f0c594464202
ttkoma <k.lapeev@gmail.com>
2022-08-11 03:37:01 +0300

    isDesktop - provide/inject

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-app id='app'>
-    <v-main class='grey lighten-2'>
+  <v-app id="app">
+    <v-main class="grey lighten-2">
       <PageContainer>
-        <TheNote v-for='(item,i) in state' :id='`note-${i}`' :key='i' :rounded='isDesktop' v-bind='item' />
+        <TheNote v-for="(item, i) in state" :id="`note-${i}`" :key="i" v-bind="item" />
       </PageContainer>
 
       <PageContainer>
         <v-footer app dark>
-          <NoteInput @input='onNoteInput' />
+          <NoteInput @input="onNoteInput" />
         </v-footer>
       </PageContainer>
     </v-main>
@@ -15,48 +15,48 @@
 </template>
 
 <script setup>
-import TheNote from './components/Note/TheNote.vue'
-import NoteInput from './components/Note/NoteInput.vue'
-import { computed, nextTick, provide, ref } from 'vue'
-import { useVuetify } from './composables/useVuetify'
-import { PageContainer } from './components/Ui'
+import TheNote from "./components/Note/TheNote.vue"
+import NoteInput from "./components/Note/NoteInput.vue"
+import { computed, nextTick, provide, ref } from "vue"
+import { useVuetify } from "./composables/useVuetify"
+import { PageContainer } from "./components/Ui"
 
 const state = ref([
-
   {
-  comment: 200,
-  editable: true,
-  fileSize: 30,
-  important: true
-},
+    comment: 200,
+    editable: true,
+    fileSize: 30,
+    important: true,
+  },
   {
     comment: 50,
     editable: false,
     fileSize: 0,
-    important: true
+    important: true,
   },
   {
     comment: 0,
     editable: false,
     fileSize: 58,
-    important: false
+    important: false,
   },
   {
     comment: 100,
     editable: false,
     fileSize: 0,
-    important: false
-  }
+    important: false,
+  },
 ])
 const { breakpoint, goTo } = useVuetify()
-const isDesktop = computed(() => breakpoint.smAndUp )
+const isDesktop = computed(() => breakpoint.smAndUp)
+provide("isDesktop", isDesktop)
 
 const onNoteInput = (event) => {
   state.value.push({
     comment: event.text.length,
     editable: true,
     important: event.isImportant,
-    fileSize: event.file ? 30 : 0
+    fileSize: event.file ? 30 : 0,
   })
 
   // После добавления елемента в DOM прокрутим окно до него
@@ -64,9 +64,4 @@ const onNoteInput = (event) => {
     goTo(`#note-${state.value.length - 1}`)
   })
 }
-
-provide("isDesktop", isDesktop)
-
 </script>
-
-
--- src/components/Note/TheNote.vue
+++ src/components/Note/TheNote.vue
@@ -1,55 +1,50 @@
 <template>
-  <div class='the-note mb-3'>
-    <v-card :loading1='important' :tile='!rounded' class='mx-auto' max-width='800' outlined>
-
+  <div class="the-note mb-3">
+    <v-card :loading="important" :tile="!isDesktop" class="mx-auto" max-width="800" outlined>
       <!-- ВАРИАНТ-1 Оформление Важного комментария:   -->
       <template #progress>
-        <v-progress-linear :value='100' color='warning' height='3' />
+        <v-progress-linear :value="100" color="warning" height="3" />
       </template>
       <!-- ШАПКА -->
       <v-list-item dense>
-
-        <v-list-item-avatar color='blue-grey lighten-3' size='36' style='padding: 2px'>
-          <v-avatar color='grey lighten-3' size='100%'>
-            <v-icon size='32'>
-              mdi-account
-            </v-icon>
+        <v-list-item-avatar color="blue-grey lighten-3" size="36" style="padding: 2px">
+          <v-avatar color="grey lighten-3" size="100%">
+            <v-icon size="32"> mdi-account </v-icon>
           </v-avatar>
         </v-list-item-avatar>
 
-        <v-list-item-content >
-          <v-list-item-title class='font-weight-medium blue-grey--text text--darken-3'> Лапеев Константин</v-list-item-title>
-          <v-list-item-subtitle class='blue-grey--text text--darken-1'>
+        <v-list-item-content>
+          <v-list-item-title class="font-weight-medium blue-grey--text text--darken-3">
+            Лапеев Константин</v-list-item-title
+          >
+          <v-list-item-subtitle class="blue-grey--text text--darken-1">
             <span>10.08.2022 22:30</span>
           </v-list-item-subtitle>
         </v-list-item-content>
 
-        <v-list-item-action class='flex-row align-center'>
-
+        <v-list-item-action class="flex-row align-center">
           <!-- ВАРИАНТ-2 Оформление Важного комментария:   -->
-          <v-icon v-show='important' color='warning'>mdi-alert-circle</v-icon>
+          <v-icon v-show="important" color="warning">mdi-alert-circle</v-icon>
 
-          <template v-if='editable'>
-            <v-btn color='blue-grey lighten-2' icon small class='ml-2'>
+          <template v-if="editable">
+            <v-btn color="blue-grey lighten-2" icon small class="ml-2">
               <v-icon>mdi-pencil</v-icon>
             </v-btn>
 
-            <v-btn  color='blue-grey lighten-2' icon small>
+            <v-btn color="blue-grey lighten-2" icon small>
               <v-icon>mdi-trash-can-outline</v-icon>
             </v-btn>
           </template>
         </v-list-item-action>
-
       </v-list-item>
 
-
       <!-- ТЕКС КОММЕНТАРИЯ -->
-      <template v-if='comment'>
+      <template v-if="comment">
         <v-divider inset />
 
         <v-list-item dense>
           <v-list-item-content>
-            <v-list-item-title class='text-body-2 text-wrap' style='line-height: 18px'>
+            <v-list-item-title class="text-body-2 text-wrap" style="line-height: 18px">
               {{ lorem(comment) }}
             </v-list-item-title>
           </v-list-item-content>
@@ -57,58 +52,55 @@
       </template>
 
       <!--  ВЛОЖЕНИЕ     -->
-      <div v-if='fileSize'>
+      <div v-if="fileSize">
         <v-divider inset />
 
-        <v-list-item dense @click='onClickDownload'>
+        <v-list-item dense @click="onClickDownload">
           <v-list-item-content>
-            <v-list-item-title class='font-weight-regular green--text'>
+            <v-list-item-title class="font-weight-regular green--text">
               {{ lorem(fileSize) }}
             </v-list-item-title>
           </v-list-item-content>
 
-          <v-list-item-action-text class='blue-grey--text'>
-            <span class='text-caption'> {{ fileSize * 24 }} Кб</span>
-            <v-icon color='green darken-1' >mdi-download</v-icon>
-
+          <v-list-item-action-text class="blue-grey--text">
+            <span class="text-caption"> {{ fileSize * 24 }} Кб</span>
+            <v-icon color="green darken-1">mdi-download</v-icon>
           </v-list-item-action-text>
         </v-list-item>
-
       </div>
-
     </v-card>
   </div>
 </template>
 
 <script setup>
-const loremText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
-  'Aspernatur consectetur dignissimos hic iure minus nam obcaecati temporibus unde. ' +
-  'Accusamus aliquid aperiam ducimus eligendi fuga hic illo, ipsa minima molestias neque nesciunt nobis repellat soluta tenetur voluptates? ' +
-  'Culpa deleniti eligendi esse explicabo fugit harum maiores necessitatibus qui quos, ratione, sit ullam.\n'
+import { inject } from "vue"
+
+const isDesktop = inject("isDesktop")
+
+const loremText =
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. " +
+  "Aspernatur consectetur dignissimos hic iure minus nam obcaecati temporibus unde. " +
+  "Accusamus aliquid aperiam ducimus eligendi fuga hic illo, ipsa minima molestias neque nesciunt nobis repellat soluta tenetur voluptates? " +
+  "Culpa deleniti eligendi esse explicabo fugit harum maiores necessitatibus qui quos, ratione, sit ullam.\n"
 
 defineProps({
   comment: {
-    type: Number
+    type: Number,
   },
   editable: {
-    type: Boolean
+    type: Boolean,
   },
   important: {
-    type: Boolean
+    type: Boolean,
   },
   fileSize: {
-    type: Number
-  },
-  rounded: {
-    type: Boolean
+    type: Number,
   },
   owned: {
-    type: Boolean
-  }
+    type: Boolean,
+  },
 })
 
 const lorem = (length) => loremText.substring(0, length)
-const onClickDownload = () => alert('Скачиваем')
-
-
+const onClickDownload = () => alert("Скачиваем")
 </script>
